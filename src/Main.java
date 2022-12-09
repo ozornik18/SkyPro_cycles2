277520a58cca3708b48db10fa3e5f990e0f0f668
Константин <kuz18rus@ya.ru>
2022-12-09 13:40:26 +0400

    задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+
+    }
+    public static void task1() {
+        System.out.println("Задача 1");
+        int contribution = 15_000;
+        double total = 0;
+        double percentagesInMonth = 0.01;
+        for (int i = 1; ; i++) {
+            while (total <= 2_459_000) {
+                total = total + contribution * percentagesInMonth;
+                total = total + contribution;
+                System.out.println("Месяц " + i + " сумма накоплений равна " + total + " рублей");
+            }
+        }
+    }
+    public static void task2() {
+        System.out.println("Задача 2");
     }
 }
