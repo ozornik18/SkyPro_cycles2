7e335158f7ae26ea9ace851eef0ccb5d311d7094
Константин <kuz18rus@ya.ru>
2022-12-09 14:26:29 +0400

    задание 2 выполнено

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
+        task3();
 
     }
     public static void task1() {
@@ -31,4 +32,6 @@ public class Main {
             }
             System.out.println();
         }
+    public static void task3() {
+        System.out.println("Задача 3");
 }
