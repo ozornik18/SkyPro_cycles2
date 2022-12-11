5bffd95002fe7024b014ca892c5d6cd8ded5d954
Константин <kuz18rus@ya.ru>
2022-12-11 14:08:50 +0400

    задание 7 выполнено

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,8 @@ public class Main {
         task4();
         task5();
         task6();
+        task7();
+ //       task8();
     }
 
     public static void task1() {
@@ -13,21 +15,23 @@ public class Main {
         int contribution = 15_000;
         int total = 0;
         int month = 0;
+
         while (total <= 2_459_000) {
-            month = month + 1;
-            total = total + contribution/100;
+            month++;
+            total = total + contribution / 100;
             total = total + contribution;
             System.out.println("Месяц " + month + ", сумма накоплений равна " + total + " рублей");
-    }
+        }
     }
 
     public static void task2() {
         System.out.println("Задача 2");
         int i = 0;
+
         while (i < 10) {
             i++;
             System.out.print(i + " ");
-    }
+        }
         System.out.println();
 
         for (int x = 10; x >= 1; x--) {
@@ -38,15 +42,15 @@ public class Main {
 
     public static void task3() {
         System.out.println("Задача 3");
-        double populationInYears = 12_000_000;
-        double birthRateInYears = 0.17;
-        double mortalityInYears = 0.08;
-        for (int i = 1; i <= 10; i++) {
-            System.out.print("Год " + i);
-            double birthRate = birthRateInYears * populationInYears;
-            double mortality = mortalityInYears * populationInYears;
-            double population = populationInYears + birthRate - mortality;
-                System.out.println(", Численность населения = " + population + populationInYears);
+        int populationInYear = 12_000_000;
+        int birthRate = 17;
+        int mortality = 8;
+        int growth = (birthRate - mortality) * 1000;
+        int year = 0;
+        while (year < 10) {
+            populationInYear = populationInYear + growth;
+            year++;
+            System.out.println("Год " + year + ", Численность населения составляет " + populationInYear);
         }
     }
 
@@ -59,10 +63,11 @@ public class Main {
         while (total <= 12_000_000) {
             total = total + contribution * percentagesInMonth;
             total = total + contribution;
-            month = month + 1;
-            System.out.println("Месяц " + month +  ", сумма накоплений равна " + total + " рублей");
+            month++;
+            System.out.println("Месяц " + month + ", сумма накоплений равна " + total + " рублей");
+        }
     }
-    }
+
     public static void task5() {
         System.out.println("Задача 5");
         int contribution = 15_000;
@@ -72,12 +77,13 @@ public class Main {
         while (total <= 12_000_000) {
             total = total + contribution * percentagesInMonth;
             total = total + contribution;
-            month = month + 1;
+            month++;
             if (month % 6 == 0) {
-                System.out.println("Месяц " + month +  ", сумма накоплений равна " + total + " рублей");
+                System.out.println("Месяц " + month + ", сумма накоплений равна " + total + " рублей");
             }
         }
     }
+
     public static void task6() {
         System.out.println("Задача 6");
         int contribution = 15_000;
@@ -87,10 +93,21 @@ public class Main {
         while (month <= 108) {
             total = total + contribution * percentagesInMonth;
             total = total + contribution;
-            month = month + 1;
+            month++;
             if (month % 6 == 0) {
-                System.out.println("Месяц " + month +  ", сумма накоплений равна " + total + " рублей");
+                System.out.println("Месяц " + month + ", сумма накоплений равна " + total + " рублей");
             }
         }
     }
+
+    public static void task7() {
+        System.out.println("Задача 7");
+        int friday = 2;
+        int day = 31;
+        while (friday <= 31) {
+            System.out.println("Сегодня пятница, " + friday + "-е число. Необходимо подготовить отчет");
+            friday += 7;
+        }
+    }
+
 }
